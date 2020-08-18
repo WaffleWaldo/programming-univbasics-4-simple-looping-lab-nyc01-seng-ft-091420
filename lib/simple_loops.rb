211ede45bf7560fef0bd2b59d1e0bad589fe30b5
Oswaldo Ortiz <oswaldoortiz0519@gmail.com>
2020-08-18 18:10:08 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,3 +16,24 @@ def loop_message_n_times(string, int)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0 
+  
+  while counter < array.length do 
+    puts array[counter]
+    counter += 1 
+  end 
+end
+
+def return_string_array(array)
+  counter = 0 
+  new_array = []
+  
+  while counter < array.length do
+    new_array.push(array[counter].to_s)
+    counter += 1
+  end
+  return new_array
+end
+  
